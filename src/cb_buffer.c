17779b5becf8912285ecbce718bc475d92f93329
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-03-04 16:31:12 -0800

    remove dangerous code

--- src/cb_buffer.c
+++ src/cb_buffer.c
@@ -34,8 +34,6 @@ on_buffer_osd_redact (GstPad * pad, GstPadProbeInfo * info)
   NvDsMetaList * l_frame = NULL;
   NvDsMetaList * l_obj = NULL;
   NvDsBatchMeta *batch_meta = gst_buffer_get_nvds_batch_meta (buf);
-  // FILE *bbox_params_dump_file = NULL;
-  // gchar bbox_file[1024] = { 0 };
 
   for (l_frame = batch_meta->frame_meta_list; l_frame != NULL;
       l_frame = l_frame->next) {
@@ -44,19 +42,8 @@ on_buffer_osd_redact (GstPad * pad, GstPadProbeInfo * info)
     
     if (frame_meta == NULL) {
       GST_WARNING("NvDS Meta contained NULL meta");
-      // TODO(mdegans): get frame number from metadata instead
-      // frame_number++;
       return GST_PAD_PROBE_OK;
     }
-
-    // TODO(mdegans): re-enable this at some point, but open the file
-    // outside the callback, writing to json lines or something instead
-    // of a text file for frame.
-    // if (OUTPUT_KITTI) {
-    //   g_snprintf (bbox_file, sizeof (bbox_file) - 1, "%s/%06d.txt",
-    //       OUTPUT_KITTI, frame_number);
-    //     bbox_params_dump_file = fopen (bbox_file, "w");
-    // }
 
     for (l_obj = frame_meta->obj_meta_list; l_obj != NULL;
         l_obj = l_obj->next) {
@@ -88,23 +75,7 @@ on_buffer_osd_redact (GstPad * pad, GstPadProbeInfo * info)
         rect_params->bg_color.blue = 0.56;
         rect_params->bg_color.alpha = 1.0;
       }
-    //   if (bbox_params_dump_file) {
-    //     int left = (int) (rect_params->left);
-    //     int top = (int) (rect_params->top);
-    //     int right = left + (int) (rect_params->width);
-    //     int bottom = top + (int) (rect_params->height);
-    //     int class_index = obj_meta->class_id;
-    //     char *text = pgie_classes_str[obj_meta->class_id];
-    //     fprintf (bbox_params_dump_file,
-    //         "%s 0.0 0 0.0 %d.00 %d.00 %d.00 %d.00 0.0 0.0 0.0 0.0 0.0 0.0 0.0\n",
-    //         text, left, top, right, bottom);
-    //   }
     }
-    // if (bbox_params_dump_file) {
-    //   fclose (bbox_params_dump_file);
-    //   bbox_params_dump_file = NULL;
-    // }  
   }
-  // frame_number++;
   return GST_PAD_PROBE_OK;
 }
