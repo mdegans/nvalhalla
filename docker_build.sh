4eac43ec78efbd82b6f33a7c5990a26f575f6687
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-23 00:20:23 +0000

    make script more flexible * for use in other projects

--- docker_build.sh
+++ docker_build.sh
@@ -4,13 +4,14 @@ set -ex
 
 # change this if you fork the repo and want to push you own image
 readonly AUTHOR="mdegans"
+readonly PROJ_NAME="nvalhalla"
 
 # this is so much more arcane than python, but people frown on build scripts written in python
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly DOCKERFILE="$THIS_DIR/Dockerfile"
 readonly VERSION=$(head -n 1 $THIS_DIR/VERSION)
-readonly TAG_BASE="$AUTHOR/nvalhalla"
+readonly TAG_BASE="$AUTHOR/$PROJ_NAME"
 readonly TAG_FULL="$TAG_BASE:$VERSION"
 
 echo "Building $TAG_FULL from $DOCKERFILE"
