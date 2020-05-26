abc70e4848a77a6b05d3c2977fbf37a3bf0e476b
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-26 23:23:16 +0000

    tag from git brach * unless master in which case tag is "latest"

--- docker_build.sh
+++ docker_build.sh
@@ -9,6 +9,10 @@ readonly PROJ_NAME="nvalhalla"
 # this is so much more arcane than python, but people frown on build scripts written in python
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TAG_SUFFIX=$(git rev-parse --abbrev-ref HEAD)
+if [[ $TAG_SUFFIX == "master" ]]; then
+    TAG_SUFFIX="latest"
+fi
 readonly DOCKERFILE="$THIS_DIR/Dockerfile"
 readonly VERSION=$(head -n 1 $THIS_DIR/VERSION)
 readonly TAG_BASE="$AUTHOR/$PROJ_NAME"
@@ -19,4 +23,4 @@ echo "Building $TAG_FULL from $DOCKERFILE"
 docker build --rm -f $DOCKERFILE \
     -t $TAG_FULL \
     $THIS_DIR
-docker tag "$TAG_FULL" "$TAG_BASE:latest"
+docker tag "$TAG_FULL" "$TAG_BASE:$TAG_SUFFIX"
